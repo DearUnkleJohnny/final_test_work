f86dad0b6e50f6d478c9c231ae5f945bc7cb13d6
DearUnkleJohnny <shuma.lov@mail.ru>
2023-01-14 15:17:17 +0300

    created a sort method

--- Final test work.cs
+++ Final test work.cs
@@ -1,16 +1,5 @@
-﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
-// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
-// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
-
-// Примеры:
-// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
-// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
-// [“Russia”, “Denmark”, “Kazan”] → []
-
-
-
-
-string[] array = new string[] { "007", "xy", "2517", "qwerty", "7" };
+﻿string[] arrayOne = new string[] { "007", "xy", "2517", "qwerty", "7" };
+string[] arrayTwo = new string[arrayOne.Length];
 
 void PrintArray(string[] array)
 {
@@ -20,6 +9,14 @@ void PrintArray(string[] array)
     }
 }
 
-Console.WriteLine("Заданный массив:");
-PrintArray(array);
+void ConvertArray(string[] array1, string[] array2)
+{
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[i] = array1[i];
+        }
+    }
+}
 
